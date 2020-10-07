979fd84c148d95b3db724f481eadc96fd17cdff6
Jacob Bare <jacob@limit0.io>
2020-10-06 20:49:38 -0500

    Create logo component

---
+++ app/components/logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img
+    src="/logo.png"
+    alt="High Oleic&reg;: Innovation in every pod"
+  >
+  <!-- src="https://tailwindui.com/img/logos/workflow-logo-on-dark.svg" -->
+</template>
