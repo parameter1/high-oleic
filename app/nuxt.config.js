c095063b179480ba9573cfe6c584a2a3708ae24d
Jacob Bare <jacob@limit0.io>
2020-10-06 21:07:51 -0500

    Set title template for all pages

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'High Oleic',
+    titleTemplate: (chunk) => (chunk ? `${chunk} - High Oleic Soy` : 'High Oleic Soy'),
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
