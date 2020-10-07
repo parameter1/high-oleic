ea2868fdd69403e86ab1a71f6bd24d7cc72ce37f
Jacob Bare <jacob@limit0.io>
2020-10-06 19:39:43 -0500

    Use left menu in default layout; set nav items

--- app/components/left-menu/nav-items.js
+++ app/components/left-menu/nav-items.js
@@ -1,1 +1,31 @@
-export default [];
+export default [
+  {
+    label: 'Why High Oleic',
+    to: '/',
+    exact: true,
+  },
+  {
+    label: 'Calculate Profit Potential',
+    to: '/calculate-profit-potential',
+  },
+  {
+    label: 'Find Your Elevator',
+    to: '/find-you-elevator',
+  },
+  {
+    label: 'Find Your Seed',
+    to: '/find-your-seed',
+  },
+  {
+    label: 'Knowledge Center',
+    to: '/knowledge-center',
+  },
+  {
+    label: 'Market Analysis',
+    to: '/market-analysis',
+  },
+  {
+    label: 'Partner Links',
+    to: '/partner-links',
+  },
+];
