e60ee43fab36699e8c6030d5b83b07b9eb29e789
Jacob Bare <jacob@limit0.io>
2020-10-06 20:10:00 -0500

    Create open menu button component

---
+++ app/components/open-menu-button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    :class="classes"
+    aria-label="Open left menu"
+    @click="$emit('click', $event)"
+  >
+    <icon-menu class="h-6 w-6" />
+  </button>
+</template>
+
+<script>
+import IconMenu from './icons/menu-alt-2-md.vue';
+
+export default {
+  components: { IconMenu },
+
+  data: () => ({
+    classes: [
+      'px-4',
+      'border-r',
+      'border-gray-200',
+      'text-gray-500',
+      'focus:outline-none',
+      'focus:bg-gray-100',
+      'focus:text-gray-600',
+      'md:hidden',
+    ],
+  }),
+};
+</script>
