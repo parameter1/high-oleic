15d11e03a575fc5055308970a77e54738cdbb1b5
Jacob Bare <jacob@parameter1.com>
2020-11-16 11:51:17 -0600

    Upgrade deps

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   plugins: [ui],
   theme: {
     extend: {
+      colors: {
+        'black-2c': '#342E1F',
+      },
       width: {
         '300px': '300px',
         '250px': '250px',
