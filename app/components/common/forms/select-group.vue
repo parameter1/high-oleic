fde500e303b9ba4bacacd2c0a2c808d9a527ccf8
Jacob Bare <jacob@parameter1.com>
2020-11-18 08:42:51 -0600

    Create select group component

---
+++ app/components/common/forms/select-group.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <field-label
+      :for="id"
+      class="ml-1 mb-1"
+    >
+      {{ label }}
+    </field-label>
+    <select-field
+      :id="id"
+      :class="inputClass"
+      :value="value"
+      :disabled="disabled"
+      :required="required"
+      :placeholder="placeholder"
+      :options="options"
+      @input="$emit('input', $event)"
+      @focus="$emit('focus', $event)"
+    />
+    <slot
+      v-if="showHint"
+      name="hint"
+    >
+      <p class="text-xs font-serif text-cool-gray-600 leading-5 mt-1 ml-1">
+        {{ hint }}
+      </p>
+    </slot>
+  </div>
+</template>
+
+<script>
+import FieldLabel from './label.vue';
+import SelectField from './select.vue';
+
+export default {
+  components: {
+    FieldLabel,
+    SelectField,
+  },
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      default: null,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    hint: {
+      type: String,
+      default: null,
+    },
+    inputClass: {
+      type: String,
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    showHint() {
+      return Boolean(this.$slots.hint || this.hint);
+    },
+  },
+};
+</script>
