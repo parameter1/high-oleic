6841e668579cad7a54cf1c18021616b0fb0ca567
Jacob Bare <jacob@parameter1.com>
2020-11-19 07:40:25 -0600

    Explicitly set font weight to input hint

--- app/components/common/forms/hint.vue
+++ app/components/common/forms/hint.vue
@@ -1,7 +1,7 @@
 <template>
   <p
     v-if="value"
-    class="text-sm font-serif text-cool-gray-600 leading-5 mt-1 ml-1"
+    class="text-sm font-normal font-serif text-cool-gray-600 leading-5 mt-1 px-1"
   >
     {{ value }}
   </p>
