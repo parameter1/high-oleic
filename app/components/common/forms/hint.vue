ccae8b7a65bca17816c288b7d0ecd52e461f6471
Jacob Bare <jacob@parameter1.com>
2020-11-19 13:29:05 -0600

    Allow (optional) hint slot

--- app/components/common/forms/hint.vue
+++ app/components/common/forms/hint.vue
@@ -1,10 +1,14 @@
 <template>
-  <p
-    v-if="value"
-    class="text-sm font-normal font-serif text-cool-gray-600 leading-5 mt-1 px-1"
-  >
-    {{ value }}
-  </p>
+  <div class="text-sm font-normal font-serif px-1 leading-5 mt-1">
+    <slot>
+      <span
+        v-if="value"
+        class="text-cool-gray-600"
+      >
+        {{ value }}
+      </span>
+    </slot>
+  </div>
 </template>
 
 <script>
