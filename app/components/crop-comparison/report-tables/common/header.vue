808b64c48148b2d70a4167a21c45b0e3614df5db
Jacob Bare <jacob@parameter1.com>
2020-11-19 13:28:55 -0600

    Set primary header to semi bold

--- app/components/crop-comparison/report-tables/common/header.vue
+++ app/components/crop-comparison/report-tables/common/header.vue
@@ -17,12 +17,12 @@ export default {
   computed: {
     classes() {
       const { type } = this;
-      const classes = ['font-medium', 'border-secondary-5-300'];
+      const classes = ['border-secondary-5-300'];
       if (type === 'primary') {
-        classes.push('p-4', 'border-b-2', 'text-lg', 'leading-6');
+        classes.push('p-4', 'border-b-2', 'text-lg', 'leading-6', 'font-semibold');
       }
       if (type === 'secondary') {
-        classes.push('px-4', 'py-3', 'border-t-2', 'border-b-2', 'border-secondary-5-300', 'font-medium', 'text-base', 'text-logo-green');
+        classes.push('px-4', 'py-3', 'border-t-2', 'border-b-2', 'border-secondary-5-300', 'font-medium', 'text-base', 'text-logo-green', 'font-medium');
       }
       return classes;
     },
