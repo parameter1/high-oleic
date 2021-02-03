e49ca732565e9b51ac561117f167452432c38d81
Jacob Bare <jacob@parameter1.com>
2021-02-03 16:34:56 -0600

    Enable print screens variant (for print styles)

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        print: { raw: 'print' },
+      },
       fontFamily: {
         sans: ['Calibri', ...defaultTheme.fontFamily.sans],
         serif: ['Cambria', ...defaultTheme.fontFamily.serif],
