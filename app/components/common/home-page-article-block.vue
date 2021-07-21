1269780bad309dd9544a2eef6d06a0bdcfb78d91
Shinsina <shinsina@att.net>
2021-07-21 08:45:49 -0500

    Rough draft of home-page-layout

---
+++ app/components/common/home-page-article-block.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="md:w-1/3">
+    <img :src="articleImage" width="200">
+    <h3 class=" text-xl">
+      {{ articleTitle }}
+    </h3>
+    <p class=" text-md">
+      {{ articleTease }}
+    </p>
+    <div class="md:w-1/4">
+      <btn
+        class="text-md"
+        type="button"
+        :block="isBlock"
+        color="logo-green"
+        @click="sendTo(linkLocation)"
+      >
+        {{ buttonLabel }}
+      </btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import Btn from './button.vue';
+
+export default {
+  components: {
+    Btn,
+  },
+  props: {
+    buttonLabel: {
+      type: String,
+      default: 'Read Article',
+    },
+    articleTitle: {
+      type: String,
+      default: 'Title',
+    },
+    articleTease: {
+      type: String,
+      default: 'Teaser',
+    },
+    articleImage: {
+      type: String,
+      default: 'https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png',
+    },
+    linkLocation: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    isBlock: true,
+  }),
+  methods: {
+    sendTo(location) {
+      this.$router.push(location);
+    },
+  },
+};
+</script>
---
+++ app/pages/why-high-oleic.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pt-2 pb-6 md:py-6">
+    <div class="max-w-7xl mx-auto sm:px-6 md:px-8">
+      <!-- Replace with your content -->
+      <div class="py-4">
+        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+      </div>
+      <!-- /End replace -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'About Us',
+  },
+};
+</script>
