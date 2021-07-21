4a69c06aef73b3c7fb6992135ebcce2db869bc0f
Shinsina <shinsina@att.net>
2021-07-21 08:45:49 -0500

    Rough draft of home-page-layout

---
+++ app/components/common/home-page-block.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="setDivClass">
+    <div :class="setSectionLabelSize">
+      <h1 v-if="isFirst">
+        {{ sectionLabel }}
+      </h1>
+      <h2 v-else>
+        {{ sectionLabel }}
+      </h2>
+    </div>
+    <div class="md:w-1/3">
+      <btn
+        class="text-3xl w-20"
+        type="button"
+        :block="isBlock"
+        color="logo-green"
+        @click="sendTo(linkLocation)"
+      >
+        {{ buttonLabel }}
+      </btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import Btn from './button.vue';
+
+export default {
+  components: {
+    Btn,
+  },
+  props: {
+    buttonLabel: {
+      type: String,
+      default: '',
+    },
+    sectionLabel: {
+      type: String,
+      default: '',
+    },
+    isFirst: {
+      type: Boolean,
+      default: false,
+    },
+    linkLocation: {
+      type: String,
+      required: true,
+    },
+  },
+  data: () => ({
+    isBlock: true,
+  }),
+  computed: {
+    setSectionLabelSize() {
+      if (this.isFirst) {
+        return 'h-4/6 text-5xl';
+      }
+      return 'h-4/5 text-4xl';
+    },
+    setDivClass() {
+      if (this.isFirst) {
+        return 'bg-primary-1 text-white h-5/6 md:pl-20 pt-24';
+      }
+      return 'bg-primary-2 text-white h-96 md:pl-20 pt-12';
+    },
+  },
+  methods: {
+    sendTo(location) {
+      this.$router.push(location);
+    },
+  },
+};
+
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,61 @@
 <template>
-  <div class="pt-2 pb-6 md:py-6">
-    <div class="max-w-7xl mx-auto sm:px-6 md:px-8">
-      <!-- Replace with your content -->
-      <div class="py-4">
-        <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+  <div class="h-screen overflow-y-scroll">
+    <!-- Replace with your content -->
+    <home-page-block
+      section-label="Why High Oleic?"
+      button-label="Learn More"
+      :is-first="isFirst"
+      link-location="/why-high-oleic"
+    />
+    <home-page-block
+      section-label="Calculate"
+      button-label="Profit Potential Calculator"
+      link-location="/crop-comparison"
+    />
+    <home-page-block
+      section-label="Find Your Seed"
+      button-label="Find Your Seed"
+      link-location="/find-your-seed"
+    />
+    <div class="w-full bg-primary-2 text-white md:pl-20 pt-12">
+      <h2 class="text-4xl">
+        Knowledge Center
+      </h2>
+      <div class="md:flex md:flex-row py-4">
+        <home-page-article-block />
+        <home-page-article-block />
+        <home-page-article-block />
       </div>
-      <!-- /End replace -->
     </div>
+    <div class="w-full bg-primary-2 text-white md:pl-20 pt-12">
+      <h2 class="text-4xl">
+        Market Analysis
+      </h2>
+      <div class="md:flex md:flex-row py-4">
+        <home-page-article-block />
+        <home-page-article-block />
+        <home-page-article-block />
+      </div>
+    </div>
+  <!-- /End replace -->
   </div>
 </template>
 
 <script>
+import HomePageBlock from '../components/common/home-page-block.vue';
+import HomePageArticleBlock from '../components/common/home-page-article-block.vue';
+
 export default {
-  fetch({ redirect }) {
-    redirect(301, '/crop-comparison');
+  components: {
+    HomePageBlock,
+    HomePageArticleBlock,
   },
+  data: () => ({
+    isFirst: true,
+  }),
   head: {
     title: 'Home',
   },
+
 };
 </script>
