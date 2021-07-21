90a58cb98cc528cb8ab7d0c995292ac08088b08e
Shinsina <shinsina@att.net>
2021-07-21 14:39:00 -0500

    Correct landing page layouts, stub article pages
    This also includes the completion of rough version of article objects and their respective body copies.

---
+++ app/pages/article/_articleName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-html="displayArticle[0].body" />
+  </div>
+</template>
+<script>
+import articles from '../../../server/src/articles/articles';
+
+export default {
+  data: () => ({
+    articles,
+  }),
+  computed: {
+    displayArticle() {
+      const internalArticles = this.articles.filter((item) => item.alias === this.$route.path);
+      return internalArticles;
+    },
+  },
+};
+</script>
