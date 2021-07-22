f9d3b2e67302dfd1d95aa64fde08bea76f373cd9
Shinsina <shinsina@att.net>
2021-07-22 11:32:44 -0500

    Overhaul styling on all pages

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,34 +7,47 @@
       :is-first="isFirst"
       link-location="/why-high-oleic"
     />
+    <div class="border-secondary-5-300 border-b-2" />
     <home-page-block
       section-label="Calculate"
       button-label="Profit Potential Calculator"
       link-location="/crop-comparison"
     />
+    <div class="border-secondary-5-300 border-b-2" />
     <home-page-block
       section-label="Find Your Seed"
       button-label="Find Your Seed"
       link-location="/find-your-seed"
     />
-    <div class="w-full bg-primary-2 text-white md:pl-20 pt-10">
-      <h2 class="text-4xl">
+    <div class="border-secondary-5-300 border-b-2" />
+    <div class="pt-10">
+      <h2 class="text-4xl font-semibold text-center md:text-left md:pl-20">
         Knowledge Center
       </h2>
-      <div class="py-2 grid grid-cols-1 md:grid-cols-3">
-        <home-page-article-block />
-        <home-page-article-block />
-        <home-page-article-block />
+      <div class="py-2 grid grid-cols-1 md:grid-cols-3 md:pl-20 border-secondary-5-300 border-b-2">
+        <div v-for="(article,index) in knowledgeCenterArticles" :key="article.name" class="py-4">
+          <div v-if="index < 3">
+            <home-page-article-block
+              :article-title="article.name"
+              :article-tease="article.teaser"
+              :link-location="article.alias"
+            />
+          </div>
+        </div>
       </div>
     </div>
-    <div class="w-full bg-primary-2 text-white md:pl-20 pt-10">
-      <h2 class="text-4xl">
+    <div class="pt-10">
+      <h2 class="text-4xl font-semibold text-center md:text-left md:pl-20">
         Market Analysis
       </h2>
-      <div class="py-2 grid grid-cols-1 md:grid-cols-3">
-        <home-page-article-block />
-        <home-page-article-block />
-        <home-page-article-block />
+      <div class="py-2 grid grid-cols-1 md:grid-cols-3 md:pl-20 border-secondary-5-300 border-b-2">
+        <div v-for="article in marketAnalysisArticles" :key="article.name" class="py-4">
+          <home-page-article-block
+            :article-title="article.name"
+            :article-tease="article.teaser"
+            :link-location="article.alias"
+          />
+        </div>
       </div>
     </div>
   <!-- /End replace -->
@@ -44,6 +57,7 @@
 <script>
 import HomePageBlock from '../components/common/home-page-block.vue';
 import HomePageArticleBlock from '../components/common/home-page-article-block.vue';
+import articles from '../../server/src/articles/articles';
 
 export default {
   components: {
@@ -51,8 +65,19 @@ export default {
     HomePageArticleBlock,
   },
   data: () => ({
+    articles,
     isFirst: true,
   }),
+  computed: {
+    knowledgeCenterArticles() {
+      const internalArticles = this.articles.filter((item) => item.scheduledSections.includes('knowledge-center')); // eslint-disable-line
+      return internalArticles;
+    },
+    marketAnalysisArticles() {
+      const internalArticles = this.articles.filter((item) => item.scheduledSections.includes('market-analysis')); // eslint-disable-line
+      return internalArticles;
+    },
+  },
   head: {
     title: 'Home',
   },
