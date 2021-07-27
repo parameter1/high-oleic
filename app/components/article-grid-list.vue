8ae669f3077c1397a81bba630462dd83e3f8251f
Jacob Bare <jacob@parameter1.com>
2021-07-27 17:21:53 -0500

    Create common article grid list component

---
+++ app/components/article-grid-list.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="containerClasses">
+    <slot name="grid-before" />
+    <home-page-article-block
+      v-for="article in articles"
+      :key="article.id"
+      :title="article.title"
+      :teaser="article.teaser"
+      :href="`/article/${article.slug}`"
+    />
+    <slot name="grid-after" />
+  </div>
+</template>
+
+<script>
+import { LOAD_ARTICLES_FOR_SECTION } from '../graphql/queries';
+import HomePageArticleBlock from './common/home-page-article-block.vue';
+
+export default {
+  components: {
+    HomePageArticleBlock,
+  },
+
+  apollo: {
+    articles: {
+      query: LOAD_ARTICLES_FOR_SECTION,
+      fetchPolicy: 'cache-and-network',
+      variables() {
+        return { alias: this.alias };
+      },
+      prefetch: true,
+    },
+  },
+
+  props: {
+    alias: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    articles: [],
+    containerClasses: [
+      'grid',
+      'grid-cols-1',
+      'md:grid-cols-2',
+      'lg:grid-cols-4',
+      'gap-4',
+    ],
+  }),
+};
+</script>
--- app/components/article-list-page.vue
+++ app/components/article-list-page.vue
@@ -5,40 +5,25 @@
         <h1 class="text-3xl font-semibold mb-8">
           {{ title }}
         </h1>
-        <div :class="containerClasses">
-          <slot name="grid-before" />
-          <home-page-article-block
-            v-for="article in articles"
-            :key="article.id"
-            :title="article.title"
-            :teaser="article.teaser"
-            :href="`/article/${article.slug}`"
-          />
-          <slot name="grid-after" />
-        </div>
+        <article-grid-list :alias="alias">
+          <template #grid-before>
+            <slot name="grid-before" />
+          </template>
+          <template #grid-after>
+            <slot name="grid-after" />
+          </template>
+        </article-grid-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { LOAD_ARTICLES_FOR_SECTION } from '../graphql/queries';
-import HomePageArticleBlock from './common/home-page-article-block.vue';
+import ArticleGridList from './article-grid-list.vue';
 
 export default {
   components: {
-    HomePageArticleBlock,
-  },
-
-  apollo: {
-    articles: {
-      query: LOAD_ARTICLES_FOR_SECTION,
-      fetchPolicy: 'cache-and-network',
-      variables() {
-        return { alias: this.alias };
-      },
-      prefetch: true,
-    },
+    ArticleGridList,
   },
 
   props: {
@@ -51,17 +36,6 @@ export default {
       required: true,
     },
   },
-
-  data: () => ({
-    articles: [],
-    containerClasses: [
-      'grid',
-      'grid-cols-1',
-      'md:grid-cols-2',
-      'lg:grid-cols-4',
-      'gap-4',
-    ],
-  }),
 
   computed: {
     alias() {
