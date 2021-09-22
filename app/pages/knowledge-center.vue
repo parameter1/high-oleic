1349d073a72fdf5bb83ecd0eb72d9832b9a4190b
Brian Miller <brian@parameter1.com>
2021-09-22 14:28:22 -0500

    convert pages using article list to use new wrapper logic
    move page into to rapper and call article-list into second part of the page

--- app/pages/knowledge-center.vue
+++ app/pages/knowledge-center.vue
@@ -1,13 +1,35 @@
 <template>
-  <article-list-page title="Knowledge Center" :path="$route.path" />
+  <div class="pb-6">
+    <page-intro-block
+      :section-label="title"
+      background-image-class="bg-knowledge-banner"
+      section-tagline="Knowledge is power"
+    />
+    <article-list-page :path="$route.path" />
+  </div>
 </template>
 
 <script>
 import ArticleListPage from '../components/article-list-page.vue';
+import PageIntroBlock from '../components/common/page-intro-block.vue';
 
 export default {
   components: {
     ArticleListPage,
+    PageIntroBlock,
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: 'Knowledge Center',
+    },
+  },
+
+  head() {
+    return {
+      title: this.title,
+    };
   },
 };
 </script>
--- app/pages/market-analysis.vue
+++ app/pages/market-analysis.vue
@@ -1,13 +1,33 @@
 <template>
-  <article-list-page title="Market Analysis" :path="$route.path" />
+  <div class="pb-6">
+    <page-intro-block
+      :section-label="title"
+    />
+    <article-list-page :path="$route.path" />
+  </div>
 </template>
 
 <script>
 import ArticleListPage from '../components/article-list-page.vue';
+import PageIntroBlock from '../components/common/page-intro-block.vue';
 
 export default {
   components: {
     ArticleListPage,
+    PageIntroBlock,
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: 'Market Analysis',
+    },
+  },
+
+  head() {
+    return {
+      title: this.title,
+    };
   },
 };
 </script>
