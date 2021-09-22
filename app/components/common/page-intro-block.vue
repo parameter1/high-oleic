1cdbf5eda848638decd5d556932c375aa28681d6
Brian Miller <brian@parameter1.com>
2021-09-22 14:25:44 -0500

    create new page intro block

---
+++ app/components/common/page-intro-block.vue
@@ -0,0 +1,82 @@
+<template>
+  <div :class="setDivClass">
+    <div :class="setSectionLabelSize">
+      <h1 class="font-bold italic text-white text-center text-shadow-dark">
+        {{ sectionLabel }}
+      </h1>
+      <p v-if="sectionTagline" class="font-bold text-center text-white text-shadow-dark">
+        {{ sectionTagline }}
+      </p>
+    </div>
+    <p v-if="sectionText" class="text-sm w-4/6 mb-6 mx-auto">
+      {{ sectionText }}
+    </p>
+    <div v-if="linkLocation" class="w-100% flex justify-center">
+      <btn
+        class="w-auto"
+        type="button"
+        :block="isBlock"
+        color="logo-yellow"
+        @click="sendTo(linkLocation)"
+      >
+        {{ buttonLabel }}
+      </btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import Btn from './button.vue';
+
+export default {
+  components: {
+    Btn,
+  },
+  props: {
+    buttonLabel: {
+      type: String,
+      default: '',
+    },
+    backgroundImageClass: {
+      type: String,
+      default: 'bg-home-banner',
+    },
+    sectionLabel: {
+      type: String,
+      default: '',
+    },
+    sectionTagline: {
+      type: String,
+      default: '',
+    },
+    sectionText: {
+      type: String,
+      default: '',
+    },
+    linkLocation: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    isBlock: false,
+  }),
+  computed: {
+    setSectionLabelSize() {
+      return `h-72 mb-6 flex justify-center flex-col ${this.backgroundImageClass} bg-cover bg-center`;
+    },
+    setDivClass() {
+      // if (this.isFirst) {
+      //   return 'h-5/6';
+      // }
+      return 'h-auto';
+    },
+  },
+  methods: {
+    sendTo(location) {
+      this.$router.push(location);
+    },
+  },
+};
+
+</script>
