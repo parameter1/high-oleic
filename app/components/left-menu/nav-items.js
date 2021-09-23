f774a1ee068f1d9a9095612d9ac5151577c77d6c
Brian Miller <brian@parameter1.com>
2021-09-23 14:56:47 -0500

    add elevator nav item back

--- app/components/left-menu/nav-items.js
+++ app/components/left-menu/nav-items.js
@@ -8,10 +8,7 @@ export default [
     label: 'Why High Oleic',
     to: '/why-high-oleic',
   },
-  // {
-  //   label: 'Find Your Elevator',
-  //   to: '/find-your-elevator',
-  // },
+
   // {
   //   label: 'Find Your Seed',
   //   to: '/find-your-seed',
@@ -23,5 +20,9 @@ export default [
   {
     label: 'Market Analysis',
     to: '/market-analysis',
+  },
+  {
+    label: 'Find A High Oleic Elevator',
+    to: '/find-your-elevator',
   },
 ];
